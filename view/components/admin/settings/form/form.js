237c6ad8d284df27bd48783657857507b56dde26
arikaim <info@arikaim.com>
2026-03-30 17:44:48 +0300

    =

--- view/components/admin/settings/form/form.js
+++ view/components/admin/settings/form/form.js
@@ -5,12 +5,4 @@ arikaim.component.onLoaded(function() {
         var settings = contactUsSettings.getSettings();
         options.save('contactus.form.settings',settings);
     });
-    
-    arikaim.ui.button('#save_send_message',function(element) {
-        var text = $('#send_message').val();  
-
-        return options.save('contactus.send.message',text,function(result) {           
-            arikaim.ui.getComponent('toast').show(result.message);
-        });
-    });   
 });
