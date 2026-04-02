1108508f0582fe689ea2ec10c14e20cf041dfb53
arikaim <info@arikaim.com>
2026-04-02 14:49:06 +0300

    =

---
+++ view/components/admin/view/actions/actions.js
@@ -0,0 +1,5 @@
+'use strict';
+
+arikaim.component.onLoaded(function() {
+    var dropdown = new HSDropdown(document.querySelector('#actins_dropdown'));
+});
--- view/components/admin/view/view.js
+++ view/components/admin/view/view.js
@@ -11,59 +11,55 @@ function ContactUsView() {
  
     this.init = function() {           
         this.loadMessages('contactus::admin');
-       
-        
-        //$('.actions').dropdown({});       
-         
+            
         search.init({ 
             id: 'contactus_rows',
             component: 'contactus::admin.view.rows'
         },'contactus');
 
         arikaim.ui.button('#delete_selected',function(element) {
-            return modal.confirmDelete({ 
-                title: self.getMessage('remove_selected.title'),
-                description: self.getMessage('remove_selected.content')
-            },function() {
-                var selected = arikaim.ui.getChecked('.selected-row');
-                contactUsAdmin.deleteSelected(selected,function(result) {
-                    arikaim.ui.table.removeSelectedRows(selected.selected);
-                });
-            });
+            return  arikaim.ui.getComponent('confirm_delete').open( 
+                function() {
+                    var selected = arikaim.ui.getChecked('.selected-row');
+                    contactUsAdmin.deleteSelected(selected,function(result) {
+                        arikaim.ui.table.removeSelectedRows(selected.selected);
+                    });
+                },self.getMessage('remove_selected.content'));
         });
 
         arikaim.ui.button('#select_all',function(element) {      
             arikaim.ui.selectAll(element);                
         });   
+
+        this.initRows();
     };
 
     this.initRows = function() {
         arikaim.ui.button('.view-message',function(element) {
-            var uuid = $(element).parent().attr('id');   
-            contactUsAdmin.setReaded(uuid).done(function(result) {
-                $('#' + uuid).removeClass('font-bold');
-            });       
-            return modal.alert({ 
-                title: 'Message',
-                component: {
-                    name: 'contactus::admin.view.message',
-                    params: { uuid: uuid }
-                },
-                icon: "address card outline icon"
+            var uuid = $(element).attr('uuid');              
+            
+            return arikaim.ui.loadComponent({ 
+                mountTo: 'message_details',
+                component: 'contactus::admin.view.message',
+                params: { 
+                    uuid: uuid 
+                }              
+            },function() {
+                contactUsAdmin.setReaded(uuid).done(function(result) {
+                    $('#' + uuid).removeClass('font-bold');
+                });     
             });                       
         });
 
         arikaim.ui.button('.delete-button',function(element) {
             var uuid = $(element).attr('uuid');   
 
-            return modal.confirmDelete({ 
-                title: self.messages.remove.title,
-                description: self.messages.remove.content
-            },function() {
-                contactUsAdmin.delete(uuid,function(result) {
-                    arikaim.ui.table.removeRow('#' + uuid);
-                });
-            });
+            return arikaim.ui.getComponent('confirm_delete').open( 
+                function() {
+                    contactUsAdmin.delete(uuid,function(result) {
+                        arikaim.ui.table.removeRow('#' + uuid);
+                    });
+                },self.messages.remove.content);
         });
     };
 }
@@ -72,5 +68,4 @@ var contactUsView = createObject(ContactUsView,ControlPanelView);
 
 arikaim.component.onLoaded(function() {
     contactUsView.init();
-    contactUsView.initRows();
 });
